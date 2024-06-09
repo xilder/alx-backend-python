d5238d48be09c4c7eaa3ab0edc938d2af94457bc
xilder <olabowaleayobami@gmail.com>
2024-06-09 18:23:48 +0100

    adds task 2

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+a function that concats two strings
+"""
+
+def concat(str1: str, b: str) -> str:
+    """adds two floats and returns the result"""
+    return str1 + str2
+
