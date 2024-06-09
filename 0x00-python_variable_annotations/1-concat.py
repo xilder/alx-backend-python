274a72d72c96ed05347aa843f3b280e88a273a0d
xilder <olabowaleayobami@gmail.com>
2024-06-09 18:30:14 +0100

    adds task 2

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -7,4 +7,3 @@ a function that concats two strings
 def concat(str1: str, str2: str) -> str:
     """adds two floats and returns the result"""
     return str1 + str2
-
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+returns the floor of the number given"""
+import math
+
+
+def floor(n: float) -> int:
+    """returns the floor of a float"""
+    return math.floor(n)
