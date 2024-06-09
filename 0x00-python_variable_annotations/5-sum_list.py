1f6a5a5f588f9b51a4e186dc7fecaed25556131a
xilder <olabowaleayobami@gmail.com>
2024-06-09 21:36:54 +0100

    adds task 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+a function that takes in a list of
+float and returns their sum as a float
+"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """returns the sum of the list of floats"""
+    return sum(input_list)
