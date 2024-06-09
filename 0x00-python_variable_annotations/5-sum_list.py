bd842418890387834d73a50781074d0c0659c087
xilder <olabowaleayobami@gmail.com>
2024-06-09 21:38:24 +0100

    task 4: proper annotation

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -3,8 +3,9 @@
 a function that takes in a list of
 float and returns their sum as a float
 """
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """returns the sum of the list of floats"""
     return sum(input_list)
