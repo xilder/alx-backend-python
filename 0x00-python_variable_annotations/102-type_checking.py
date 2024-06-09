a72d2ba8fa0d4423b358f9d94dcf21511f92ec26
xilder <olabowaleayobami@gmail.com>
2024-06-09 23:53:40 +0100

    update task 102: resolves mypy conflicts

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -15,8 +15,9 @@ def zoom_array(lst: Tuple, factor: int = 2) -> List:
     return zoomed_in
 
 
-array = [12, 72, 91]
+array = (12, 72, 91)
 
 zoom_2x = zoom_array(array)
 
-zoom_3x = zoom_array(array, 3)
+zoom_3x = zoom_array(array, 3)  
+
