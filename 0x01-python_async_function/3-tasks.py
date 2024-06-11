e8bf70fffca87b2f0d20d47d04ae4503b562014c
xilder <olabowaleayobami@gmail.com>
2024-06-11 16:14:46 +0100

    adds task 3

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+returns an asyncio.Task object
+"""
+import asyncio
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """returns an asyncio.Task object"""
+
+    return asyncio.create_task(wait_random(max_delay))
--- 0x01-python_async_function/main.py
+++ 0x01-python_async_function/main.py
@@ -1,8 +1,11 @@
-#!/usr/bin/env python3
+import asyncio
 
-measure_time = __import__('2-measure_runtime').measure_time
+task_wait_random = __import__('3-tasks').task_wait_random
 
-n = 5
-max_delay = 9
 
-print(measure_time(n, max_delay))
+async def test(max_delay: int) -> float:
+    task = task_wait_random(max_delay)
+    await task
+    print(task.__class__)
+
+asyncio.run(test(5))
