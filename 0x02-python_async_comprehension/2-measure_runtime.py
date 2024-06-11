4f5c584bdab61d4610960f3570256b0fdd558ae2
xilder <olabowaleayobami@gmail.com>
2024-06-11 17:34:07 +0100

    adds task 3

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Measures the runtime for the function async_comprehension
+"""
+import asyncio
+from time import time
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> int:
+    """
+    measure_time async function
+    """
+    start = time()
+    await asyncio.gather(*[async_comprehension() for _ in range(100000)])
+    return time() - start
