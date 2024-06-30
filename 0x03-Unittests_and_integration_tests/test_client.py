ac3b37acb509bbeefd2f02ae1674b7c7f7f8a031
xilder <olabowaleayobami@gmail.com>
2024-06-30 23:42:54 +0100

    adds task 5

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """test file for clients class"""
 from typing import Dict
-from unittest.mock import MagicMock, patch
+from unittest.mock import MagicMock, PropertyMock, patch
 from parameterized import parameterized
 import unittest
 from client import GithubOrgClient
@@ -25,3 +25,22 @@ class TestGithubOrgClient(unittest.TestCase):
         mocked_function.assert_called_once_with(
             f"https://api.github.com/orgs/{org}"
             )
+
+
+    def test_public_repos_url(self) -> None:
+        """
+        tests the _public_repos_url method
+        of the GithubOrgClient
+        """
+
+        with patch(
+            "client.GithubOrgClient.org",
+            new_callable=PropertyMock
+        ) as mock_org:
+            mock_org.return_value = {
+                "repos_url": "https://api.github.com/users/google/repos",
+            }
+            self.assertEqual(
+                GithubOrgClient("google")._public_repos_url,
+                "https://api.github.com/users/google/repos"
+                )
