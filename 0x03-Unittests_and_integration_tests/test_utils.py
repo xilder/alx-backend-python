f5ec2fdc0b4fe77192615689cab05fc5cd19702b
xilder <olabowaleayobami@gmail.com>
2024-06-30 01:44:01 +0100

    unittest cases for the access_nested_map function

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""unit tests for nest_map function"""
+import unittest
+from parameterized import parameterized
+
+
+access_nested_map = __import__("utils").access_nested_map
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """unittest class"""
+    parameterized.expand(
+            ({"a": {"b": 2}}, ("a", "b"), 2),
+            ({"a": {"b": 2}}, ("a",), {"b": 2}),
+            ({"a": 1}, ("a",), 1)
+        )
+
+    def test_access_nested_map(self, nested_map, path, expected_output):
+        """tests the access_nested_map function"""
+        result = access_nested_map(nested_map, path)
+        self.assertEqual(result, expected_output)
